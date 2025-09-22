abe109a263117ab9ea8022f17fa9f41c45499063
Rafael <rafadepe27@gmail.com>
2025-09-22 09:46:14 -0500

    CAMBIO

--- main.py
+++ main.py
@@ -275,4 +275,4 @@ async def delete_report(report_id: int, db: Session = Depends(get_db)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=5000, reload=True)
