760c1318a760c0403923db8504b0e6a238372a1f
Rafael <rafadepe27@gmail.com>
2025-10-03 12:08:53 -0500

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,12 +4,13 @@ module.exports = {
       name: "backend-reportes",
       cwd: "/home/ubuntu/backend-reportes",
 
-      // Ejecuta uvicorn como módulo de Python para usar el venv
-      interpreter: "/home/ubuntu/backend-reportes/.venv/bin/python",
+      // Ejecuta Uvicorn como módulo de Python (no usamos venv)
+      interpreter: "python3",
       interpreter_args: "-m",
       script: "uvicorn",
       args: "main:app --host 0.0.0.0 --port 5000",
 
+      // Producción: sin --reload
       watch: false,
       autorestart: true,
       max_restarts: 10,
