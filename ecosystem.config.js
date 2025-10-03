f1e457e1da1d89836d07addb93422a456ed38d21
Rafael <rafadepe27@gmail.com>
2025-10-03 11:31:29 -0500

    ahora

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,19 +2,21 @@ module.exports = {
   apps: [{
     name: 'reports-center',
     script: 'main.py',
+    args: '-m uvicorn main:app --host 0.0.0.0 --port 5000 --reload',
     cwd: '/home/ubuntu/backend-reportes',
-    args: 'main:app --host 0.0.0.0 --port 5000 --reload',
     instances: 1,
     autorestart: true,
-    watch: false,
+    watch: true,
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'production',
-      PORT: 5000
+      PORT: 5000,
+      PYTHONPATH: '/home/ubuntu/backend-reportes',
+      PYTHONUNBUFFERED: 1
     },
-    error_file: './logs/err.log',
-    out_file: './logs/out.log',
-    log_file: './logs/combined.log',
+    error_file: '/home/ubuntu/backend-reportes/logs/err.log',
+    out_file: '/home/ubuntu/backend-reportes/logs/out.log',
+    log_file: '/home/ubuntu/backend-reportes/logs/combined.log',
     time: true
   }]
 };
