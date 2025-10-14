77f915f09d9cc5eefd84bd993e92d395ed381e5a
Rafael <rafadepe27@gmail.com>
2025-10-14 18:29:22 -0500

    cambio del eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,16 +3,19 @@ module.exports = {
     {
       name: "backend-reportes",
       cwd: "/home/ubuntu/backend-reportes",
-
-      // Ejecuta Uvicorn como módulo de Python (no usamos venv)
-      interpreter: "python3",
-      script: "main.py",
-
-      // Producción: sin --reload
+      script: "/home/ubuntu/backend-reportes/venv/bin/uvicorn",
+      args: "main:app --host 0.0.0.0 --port 5000 --reload",
+      interpreter: "none",
       watch: false,
       autorestart: true,
       max_restarts: 10,
       max_memory_restart: "512M",
+      env: {
+        PYTHONPATH: "/home/ubuntu/backend-reportes"
+      },
+      error_file: "/home/ubuntu/backend-reportes/logs/err.log",
+      out_file: "/home/ubuntu/backend-reportes/logs/out.log",
+      log_file: "/home/ubuntu/backend-reportes/logs/combined.log",
     }
   ]
 }
